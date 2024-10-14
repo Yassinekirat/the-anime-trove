3a91d28a6c22c1ad3e12ad296408d55181087c22
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-10-14 14:27:26 +0100

    Add search, animeList, and topAnime state constants

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import Header from "./components/Header";
 import { useState, useEffect } from 'react';
 
 function App() {
+  const [search, SetSearch] = useState("");
+  const [animeList, SetAnimelist] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+
   return (
     <div className="App">
-      hello, world
+      <Header />
     </div>
   );
 }
