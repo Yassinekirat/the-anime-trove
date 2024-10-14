3e8c82a847beb2a6abc95884dd2180f72cc3f516
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-10-14 16:44:34 +0100

    more accordion documentation

--- src/components/Accordion/AccordionItem.js
+++ src/components/Accordion/AccordionItem.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const AccordionItem = ({ title, children, isOpen, onToggle }) => {
-  return (
-    <div className="accordion-item">
-      <div className="accordion-title" onClick={onToggle}>
-        <h3>{title}</h3>
-        <span>{isOpen ? '-' : '+'}</span>
+    return (
+      <div className="accordion-item">
+        {/* Title that toggles the accordion content */}
+        <div className="accordion-title" onClick={onToggle}>
+          <h3>{title}</h3>
+          {/* Indicate open/closed state with '+' or '-' */}
+          <span>{isOpen ? '-' : '+'}</span>
+        </div>
+        {/* Render content if the item is open */}
+        {isOpen && <div className="accordion-content">{children}</div>}
       </div>
-      {isOpen && <div className="accordion-content">{children}</div>}
-    </div>
-  );
-};
-
-export default AccordionItem;
+    );
+  };
+  
+  export default AccordionItem;
